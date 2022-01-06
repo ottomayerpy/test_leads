dde96768e0bb12833b10f376f824bc4524343e3e
ottomayerpy <fivesevenom@gmail.com>
2022-01-06 04:13:37 +0300

    change comment

--- test_leads/core/urls.py
+++ test_leads/core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # После проверки от ВК, заменить vk_check на vk_group_new_message
+    # После проверки от ВК, заменить метод vk_check на vk_group_new_message
     path('vk_group_new_message', views.vk_check, name='vk_group_new_message_url'),
     path('tg_group_new_message', views.tg_group_new_message, name='tg_group_new_message_url')
 ]
